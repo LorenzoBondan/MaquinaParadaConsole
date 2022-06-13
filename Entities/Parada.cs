de37717e1b109a7c3a18bbab98ec884d7a7bbf3c
LorenzoBondan <lbbondan@gmail.com>
2022-06-13 15:33:25 -0300

    Máquina parada console

---
+++ Entities/Parada.cs
@@ -0,0 +1,46 @@
+﻿using MaquinaParada.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaParada.Entities
+{
+    class Parada
+    {
+        public int Numero { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Final { get; set; }
+
+        public Parada() { }
+
+        public Parada(int numero, DateTime inicio, DateTime final)
+        {
+            if (final <= inicio)
+            {
+                throw new DomainException("O horário final deve ser maior que o inicial.");
+            }
+
+            Numero = numero;
+            Inicio = inicio;
+            Final = final;
+        }
+
+        public double Duracao()
+        {
+            TimeSpan duracao = Final.Subtract(Inicio);
+            return (int)duracao.TotalMinutes;
+        }
+
+        public override string ToString()
+        {
+            return "\nParada: " + Numero
+                + " - Início: " + Inicio.ToString("HH:mm")
+                + " - Final: " + Final.ToString("HH:mm")
+                + " - Duração: " + Duracao() + " minutos.";
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaquinaParada.Entities.Exceptions;
+using MaquinaParada.Entities;
+using System.Linq.Expressions;
+
+namespace MaquinaParada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Parada> lista = new List<Parada>();
+
+            Console.WriteLine("Quantas paradas?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                try
+                {
+                    Console.Write($"Número da parada {i}: ");
+                    int numero = int.Parse(Console.ReadLine());
+
+                    Console.Write($"Horário de início da parada {i}: ");
+                    DateTime inicio = DateTime.Parse(Console.ReadLine());
+
+                    Console.Write($"Horário de término da parada {i}: ");
+                    DateTime final = DateTime.Parse(Console.ReadLine());
+
+                    Parada parada = new Parada(numero, inicio, final);
+
+                    lista.Add(parada);
+
+                }
+                catch (FormatException e) // colocar letra ao invés de número
+                {
+                    Console.WriteLine("Erro de inserção de dados: " + e.Message);
+                }
+                catch (DomainException e)
+                {
+                    Console.WriteLine("Erro de tempo de parada: " + e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erro não esperado: " + e.Message);
+                }
+            }
+
+            // imprimindo 
+            Console.WriteLine();
+            foreach (Parada p in lista)
+            {
+                Console.WriteLine(p);
+            }
+
+            int maiorparada = (int)lista.Max(t => t.Duracao()); // SÓ TRAZ A DURAÇÃO
+            Console.WriteLine("\nMaior parada: " + maiorparada);
+
+            Console.ReadKey();
+        }
+    }
+}
